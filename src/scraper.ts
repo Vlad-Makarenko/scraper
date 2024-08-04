9f40ddf547546702c92000f894f186dccc832c57
Vlad-Makarenko <makrenkovladok@gmail.com>
2024-08-04 22:26:18 +0300

    Catalog interface added

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,10 +1,16 @@
 import puppeteer from 'puppeteer';
 
-export async function scraper() {
+interface Catalog {
+	title: string;
+	link: string;
+	startTime: string;
+	endTime: string;
+}
+
+export async function scraper(): Promise<Catalog[]> {
 	const browser = await puppeteer.launch({ headless: false });
 	const page = await browser.newPage();
 	await page.goto('https://www.tus.si/#s2');
-	await page.waitForSelector('div.card.card-catalogue');
 	const catalogs = await page.$$eval('div.card.card-catalogue', elements => {
 		return elements.map(element => {
 			const title = element.querySelector('h3')?.textContent?.trim() || '';
